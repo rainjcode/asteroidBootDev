b11e64dc28cd315c4dfe8b8dfa7ae5f18f20b070
rainjcode <jcode2650@gmail.com>
2026-06-27 08:23:04 -0500

    Essentail functions for a running game

---
+++ asteroid.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+from logger import log_event
+from circleshape import CircleShape
+from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        split1_velocity = self.velocity.rotate(angle)
+        split2_velocity = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        split1 = Asteroid(self.position.x, self.position.y, new_radius)
+        split2 = Asteroid(self.position.x, self.position.y, new_radius)
+        split1.velocity = split1_velocity
+        split2.velocity = split2_velocity
